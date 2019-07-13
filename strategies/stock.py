62d5228ed6b54837f2ade38a26cd6830038a2a79
Max Fu <max.fu.letian@gmail.com>
2019-07-13 12:30:36 -0700

    local

---
+++ strategies/stock.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+def trade(exchange):
+    data = exchange.last_data
+    delta_t_history = exchange.delta
+    trades = []
+    if data['type'] == 'book' and data['symbol'] == 'BOND':
+        bids = data['buy']
+        for price, size in bids:
+            if price > 1000:
+                trades.append(('SELL', 'BOND', price, size))
+
+        asks = data['sell']
+        for price, size in asks:
+            if price < 1000:
+                trades.append(('BUY', 'BOND', price, size))
+    return trades
