fc56efcfc197ce31ff377b431cff41ab170a0cd4
yuhongc <mikechen212@gmail.com>
2019-07-13 15:54:11 -0700

    change priceing

--- strategies/convert_adr.py
+++ strategies/convert_adr.py
@@ -22,14 +22,14 @@ def trade(exchange):
     diff = adr_estimate - ori_estimate
     if diff > 10:
         size = 1
-        trades.append(("BUY", "VALBZ", int(exchange.), size))
+        trades.append(("BUY", "VALBZ", int(exchange.trade_prices["VALBZ"]), size))
         trades.append(("CONVERT", "BUY", "VALE", size))
-        trades.append(("SELL", "VALE", int(adr_estimate-10), size + exchange.holdings["VALE"]))
+        trades.append(("SELL", "VALE", int(exchange.trade_prices["VALE"]), size + exchange.holdings["VALE"]))
     elif diff < 10:
         size = 1
-        trades.append(("SELL", "VALBZ", int(ori_estimate-10), size + exchange.holdings["VALBZ"]))
+        trades.append(("SELL", "VALBZ", int(exchange.trade_prices["VALBZ"]), size + exchange.holdings["VALBZ"]))
         trades.append(("CONVERT", "SELL", "VALE", size))
-        trades.append(("BUY", "VALE", int(adr_estimate+5), size))
+        trades.append(("BUY", "VALE", int(exchange.trade_prices["VALE"]), size))
     return trades
 
 def fair_price_estimate(buy, sell):
