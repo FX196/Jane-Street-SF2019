3fb8d33886ed42759487c881173b4552fa13dcbd
yuhongc <mikechen212@gmail.com>
2019-07-13 14:18:02 -0700

    slow

--- Connection.py
+++ Connection.py
@@ -121,7 +121,7 @@ class ExchangeConnection:
                         if data["order_id"] == id:
                             self.current_orders.pop(index)
                             break
-        if self.counter % 50 == 0:
+        if self.counter % 200 == 0:
             print(self.holdings)
             print(self.current_orders)
             print(self.sent_orders)
