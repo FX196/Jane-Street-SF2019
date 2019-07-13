e2956fbe924d7cb2cd53adc2dd3e32eefae6fa1c
Max Fu <max.fu.letian@gmail.com>
2019-07-13 11:00:12 -0700

    added stats file

---
+++ strategies/stats.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+"""
+Return the standard deviation of the current distribution
+"""
+def std(distribution):
+    return np.std(distribution)
+
+"""
+return the mean of the distribution
+"""
+def mean(distribution):
+    return np.mean(distribution)
+
+
