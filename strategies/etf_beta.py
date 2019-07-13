06da390f4e61dba6de795560dca4bbd766635ab6
cal-JackZhao <cal_zhk@berkeley.edu>
2019-07-13 13:56:08 -0700

    etf_beta-3

--- strategies/etf_beta.py
+++ strategies/etf_beta.py
@@ -1,13 +1,16 @@
 import numpy as np
 def trade(exchange):
     trades=[]
-    bond = exchange.t_now["BOND"]
-    gs = exchange.t_now["GS"]
-    ms = exchange.t_now["MS"]
-    wfc = exchange.t_now["WFC"]
-    xlf = exchange.t_now["XLF"][-1]
-    if len(bond)<6 or len(gs)<6 or len(ms)<6 or len(wfc)<6:
-        return trades
+    try:
+        bond = exchange.t_now["BOND"]
+        gs = exchange.t_now["GS"]
+        ms = exchange.t_now["MS"]
+        wfc = exchange.t_now["WFC"]
+        xlf = exchange.t_now["XLF"][-1]
+    except:
+        return []
+    if len(bond)<5 or len(gs)<5 or len(ms)<5 or len(wfc)<5:
+        return []
     ave = 3 * np.mean(bond[-5:]) + 2 * np.mean(gs[:-5]) + 3 * np.mean(ms[:-5]) + 2 * np.mean(wfc[:-5])
     if (xlf < ave):
         trades.append(("BUY", "XLF", ave, 30))
