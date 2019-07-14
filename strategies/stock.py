71572a2f9032c9cc1c36cfffaee364ca33e8751d
Max Fu <max.fu.letian@gmail.com>
2019-07-13 18:17:10 -0700

    local

--- strategies/stock.py
+++ strategies/stock.py
@@ -6,7 +6,7 @@ def trade(exchange):
     data = exchange.last_data
     delta_t_history = exchange.delta_t
     total_trade = exchange.t_now
-    stocks = ['GS', 'MS', 'WFC', 'VALBZ']
+    stocks = ['GS', 'MS', 'WFC']
     current_holding = exchange.holdings
     buy_price = {} # process buy price when purchase
 
